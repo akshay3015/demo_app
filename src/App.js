77608a934a9c2c486382ce72a7ab0f2c2175d474
akshay shahane <akshay.shahane13@gmail.com>
2022-02-28 17:24:13 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useEffect, useState } from "react";
+
+
+const data1 = [
+  { movieName: "Die Hard", genre: "Action", stock: "5", rate: "2.5" },
+  { movieName: "Get Out", genre: "Thriller", stock: "8", rate: "3.5" },
+  { movieName: "Airplane", genre: "Comedy", stock: "7", rate: "3.5" },
+  { movieName: "Wedding Crashers", genre: "Comedy", stock: "7", rate: "3.5" },
+  { movieName: "Gone Girl", genre: "Thriller", stock: "7", rate: "4.5" },
+  { movieName: "The Sixth Sense", genre: "Thriller", stock: "4", rate: "3.5" },
+]
+
+
 
 function App() {
+  const [data, updatedData] = React.useState(
+    data1
+  );
+
+ 
+function handleClick(index) {
+      console.log(index);
+     
+      
+    
+      
+      //  console.log(data1.splice(index,1));
+      //   updatedData(data1);
+
+        updatedData(()=>{
+          let tempArray = data.filter(temp => temp.movieName!= data[index].movieName);
+          console.log(tempArray);
+          return tempArray;
+        });
+    
+
+     }
+
+ // does not work style="overflow-x:auto;"
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" >
+      {
+        
+      }
+      <table>
+        <tr>
+          <th>Title</th>
+          <th>Genre</th>
+          <th>Stock</th>
+          <th>Rate</th>
+          <th>  </th>
+        </tr>
+      {  data.length < 1 && <h2>Data not found</h2>}
+        {
+          
+        data.map((val, key) => {
+          console.log(key);
+        
+          return (
+         
+            <tr key={key}>
+              <td>{val.movieName}</td>
+              <td>{val.genre}</td>
+              <td>{val.stock}</td>
+              <td>{val.rate}</td>
+              <td><button onClick={() =>handleClick(key)} class="button">
+                Delete
+              </button></td>
+            </tr>
+          )
+        })}
+      </table>
+
     </div>
   );
 }
